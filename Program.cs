c112fb10b4b095e25850750c8eae7fba3b2a2b4a
kwabenadarkwa <darkwak@live.com>
2024-10-10 11:44:44 +0100

    adds swagger ui to application

--- Program.cs
+++ Program.cs
@@ -1,6 +1,37 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Microsoft.OpenApi.Models;
 
-app.MapGet("/", () => "Hello World!");
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddEndpointsApiExplorer();
+if (builder.Environment.IsDevelopment())
+{
+    _ = builder.Services.AddSwaggerGen(static c =>
+    {
+        c.SwaggerDoc(
+            "v1",
+            new OpenApiInfo
+            {
+                Title = "Todo API",
+                Description = "Keep track of your tasks",
+                Version = "v1",
+            }
+        );
+    });
+}
+
+WebApplication app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    _ = app.UseSwagger();
+    _ = app.UseSwaggerUI(static c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Todo API V1");
+    });
+}
+
+app.MapGet("/", static () => "Hello World!");
 
 app.Run();
